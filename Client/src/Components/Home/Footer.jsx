ecc4997bba99816aa5def63d41828431df5fdb24
John Patrick Trinidad <Ptrinidad765@gmail.com>
2025-05-22 09:27:17 +0800

    fixed footer

--- Client/src/Components/Home/Footer.jsx
+++ Client/src/Components/Home/Footer.jsx
@@ -1,26 +1,33 @@
 import { FaFacebookF, FaTwitter, FaInstagram, FaGithub } from 'react-icons/fa';
+import {useNavigate} from 'react-router-dom';
 
 export default function Footer() {
+  const navigate = useNavigate();
   return (
     <footer className="bg-[#141414] text-white pt-10 pb-5 px-6 md:px-16">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-10 mb-8">
 
         {/* About */}
-        <div>
-          <h2 className="text-lg font-semibold mb-4">About</h2>
+        <div className='h-full '>
+          <h2 className="text-lg font-semibold mb-0">About</h2>
+          <div className='h-full flex flex-col gap-4'>
           <p className="text-sm text-gray-300">
             AniHive is your one-stop platform to browse airing anime, discover top-rated series, and explore the latest anime trends.
           </p>
+          <p className="text-sm text-gray-500">
+            Disclaimer: This site does not store any files on its server. All contents are provided by non-affiliated third parties.
+          </p>
+          </div>
         </div>
 
         {/* Quick Links */}
         <div>
           <h2 className="text-lg font-semibold mb-4">Quick Links</h2>
           <ul className="space-y-2 text-sm text-gray-300">
-            <li><a href="/" className="hover:text-white">Home</a></li>
-            <li><a href="/top-anime" className="hover:text-white">Schedule</a></li>
-            <li><a href="/seasonal" className="hover:text-white">Genre</a></li>
-            <li><a href="/genres" className="hover:text-white">Forum</a></li>
+            <li><button onClick={()=>navigate('/')} className="hover:text-white cursor-pointer">Anime</button></li>
+            <li><button onClick={()=>navigate('/schedule')} className="hover:text-white cursor-pointer">Schedule</button></li>
+            <li><button onClick={()=>navigate('/explore')} className="hover:text-white cursor-pointer">Explore</button></li>
+            <li><button onClick={()=>navigate('/characters')} className="hover:text-white cursor-pointer">Characters</button></li>
           </ul>
         </div>
 
