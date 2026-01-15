90694dbef7e0dc3643d9d308e76e0e1126f04518
John Patrick Trinidad <Ptrinidad765@gmail.com>
2026-01-15 10:59:13 +0800

    Removed Consumet and Mal Server to be moved to different repo

--- MalServer/routes/MalAuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { token, logout } = require('../controller/MalAuthController')
-
-const router = express.Router()
-
-router.post('/auth/mal/token', token)
-router.post('/auth/mal/logout', logout)
-
-module.exports = router
